49a0eac8ca5cab21b9646d5b67131491f51a7633
Sergiu Popescu <44298302+sergiupopescu199@users.noreply.github.com>
2026-04-29 10:11:55 +0200

    chore: update tonic dependencies (#12)

--- build.rs
+++ build.rs
@@ -4,18 +4,19 @@
 use std::path::Path;
 
 const GOOGLEAPIS_SUBMODULE_PATH: &str = "googleapis";
+const BIGTABLE_V2_PROTO_PATH: &str = "google/bigtable/v2/bigtable.proto";
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         // the server is on google side, we don't need code generation for it.
         .build_server(false)
         .compile_protos(
-            &[Path::new(GOOGLEAPIS_SUBMODULE_PATH).join("google/bigtable/v2/bigtable.proto")],
-            &[GOOGLEAPIS_SUBMODULE_PATH],
+            &[Path::new(GOOGLEAPIS_SUBMODULE_PATH).join(BIGTABLE_V2_PROTO_PATH)],
+            &[GOOGLEAPIS_SUBMODULE_PATH.into()],
         )?;
 
     Ok(())
